ffe3da211e388525672bbb3582578599ed577c67
Jeremaiah Joy Javier Villanueva <villanueva_j@ca02.lan>
2024-03-08 16:35:24 +0800

    PRJ Temps

---
+++ Project Templates/Sidebar/app.js
@@ -0,0 +1,12 @@
+const toggleBtn=document.querySelector('.sidebar-toggle')
+const closeBtn=document.querySelector('.close-btn')
+const sidebar=document.querySelector('.sidebar')
+
+toggleBtn.addEventListener('click', () =>{
+    console.log("THe button is clicked")
+    sidebar.classList.toggle('show-sidebar')
+})
+closeBtn.addEventListener('click', () =>{
+    console.log('Close button is clicked.')
+    sidebar.classList.remove('show-sidebar')
+})
